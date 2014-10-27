8dbcc94032a235f8272af3beb1f11265ae0497c9
Jorge Calderon <jcalderon@fas.harvard.edu>
2014-10-27 01:03:12 -0400

    added redis pool

--- RepastSimulationRunner/src/org/simulationsystems/csf/common/internal/messaging/interfaces/redis/RedisConnectionManager.java
+++ RepastSimulationRunner/src/org/simulationsystems/csf/common/internal/messaging/interfaces/redis/RedisConnectionManager.java
@@ -1,22 +1,36 @@
 package org.simulationsystems.csf.common.internal.messaging.interfaces.redis;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /*
  * Manages the CRUD operations to Redis. This class has no business-level knowledge.
  */
 public class RedisConnectionManager {
-	private Jedis jedis=null;
+	// private Jedis jedis=null;
+	private JedisPool jedisPool;
 
 	public void initializeRedisConnection(final String host) {
-		jedis = new Jedis(host);
+		// jedis = new Jedis(host);
+		jedisPool = new JedisPool(new JedisPoolConfig(), host);
 
 		// TODO: Throw exception is Jedis returns null?
 	}
 
 	public void postMessage(String channel, String message) {
-		jedis.set(channel, message);
-		String value = jedis.get(channel);
-		System.out.println("Set: " + value);
+		Jedis jedis = jedisPool.getResource();
+		jedis.lpush(channel, message);
+
+		System.out.println("Set: " + message);
+		System.out.println("lpop: " + jedis.lpop(channel));
+		System.out.println("lpop: " + jedis.lpop(channel));
+		
+		// FIXME: Remove
+		closePool();
+	}
+
+	public void closePool() {
+		jedisPool.destroy();
 	}
 }

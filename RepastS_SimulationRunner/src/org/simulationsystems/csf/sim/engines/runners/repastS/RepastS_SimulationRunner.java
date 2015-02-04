7aac949a318c1fd422f3de5d40fd60fce53ddf8b
Jorge Calderon <jcalderon@fas.harvard.edu>
2015-02-03 23:23:50 -0500

    added uml svg diagrams

---
+++ RepastS_SimulationRunner/src/org/simulationsystems/csf/sim/engines/runners/repastS/RepastS_SimulationRunner.java
@@ -0,0 +1,239 @@
+package org.simulationsystems.csf.sim.engines.runners.repastS;
+
+import java.io.File;
+
+import org.simulationsystems.csf.common.csfmodel.SIMULATION_TYPE;
+import org.simulationsystems.csf.common.csfmodel.csfexceptions.CsfInitializationRuntimeException;
+import org.simulationsystems.csf.sim.adapters.simengines.repastS.api.RepastS_SimulationAdapterAPI;
+import org.simulationsystems.csf.sim.adapters.simengines.repastS.api.RepastS_SimulationRunContext;
+import org.simulationsystems.csf.sim.adapters.simengines.repastS.api.RepastS_SimulationRunGroupContext;
+
+import repast.simphony.batch.BatchScenarioLoader;
+import repast.simphony.context.Context;
+import repast.simphony.engine.controller.Controller;
+import repast.simphony.engine.controller.DefaultController;
+import repast.simphony.engine.environment.AbstractRunner;
+import repast.simphony.engine.environment.ControllerRegistry;
+import repast.simphony.engine.environment.DefaultRunEnvironmentBuilder;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.environment.RunEnvironmentBuilder;
+import repast.simphony.engine.environment.RunState;
+import repast.simphony.engine.schedule.ISchedule;
+import repast.simphony.engine.schedule.Schedule;
+import repast.simphony.parameter.DefaultParameters;
+import repast.simphony.parameter.ParameterConstants;
+import repast.simphony.parameter.SweeperProducer;
+import simphony.util.messages.MessageCenter;
+
+/*
+ * The Repast AbstractRunner that programmatically runs a Repast Simulation. This class is taken
+ * from the Repast FAQ and enhanced to allow incorporation of the common simulation framework.
+ * 
+ * Based on TestRunner_2.java from:
+ * http://sourceforge.net/p/repast/repast-simphony-docs/ci/master/tree
+ * /docs/RepastFAQ/TestMain_2.java
+ * 
+ * @author
+ */
+public class RepastS_SimulationRunner extends AbstractRunner {
+	private RepastS_SimulationAdapterAPI repastS_SimulationAdapterAPI;
+
+	private RepastS_SimulationRunGroupContext repastS_SimulationRunGroupContext;
+	private RepastS_SimulationRunContext lastRepastS_SimulationRunContext;
+
+	/*
+	 * The run group context is set upon the return of the load() function
+	 */
+	public RepastS_SimulationRunGroupContext getRepastS_SimulationRunGroupContext() {
+		return repastS_SimulationRunGroupContext;
+	}
+
+	private SIMULATION_TYPE simulationRunnerType;
+
+	private boolean isStopped;
+
+	// TODO: Add public getter to use native Repast logging from the runner main
+	private static MessageCenter msgCenter = MessageCenter
+			.getMessageCenter(RepastS_SimulationRunner.class);
+
+	private RunEnvironmentBuilder runEnvironmentBuilder;
+	protected Controller controller;
+	protected boolean pause = false;
+	protected Object monitor = new Object();
+	protected SweeperProducer producer;
+	private ISchedule schedule; // level
+
+	public RepastS_SimulationRunner() {
+		runEnvironmentBuilder = new DefaultRunEnvironmentBuilder(this, true);
+		controller = new DefaultController(runEnvironmentBuilder);
+		controller.setScheduleRunner(this);
+	}
+
+	public void load(File scenarioDir, String frameworkConfigurationFileName)
+			throws Exception {
+		if (scenarioDir.exists()) {
+			BatchScenarioLoader loader = new BatchScenarioLoader(scenarioDir);
+			ControllerRegistry registry = loader.load(runEnvironmentBuilder);
+			controller.setControllerRegistry(registry);
+		} else {
+			msgCenter.error("Scenario not found", new IllegalArgumentException(
+					"Invalid scenario " + scenarioDir.getAbsolutePath()));
+			throw new CsfInitializationRuntimeException(
+					"Unable to initialize the simulation run.  Are you pointed to the right simulation configuration directory?  In Repast Simphony, it ends in .rs.  Tried using:  "
+							+ scenarioDir);
+			// return;
+		}
+		controller.batchInitialize();
+
+		// Set the Parameters across all simulation runs of this simulation
+		// HARD CODED FOR NOW
+		// TODO: Programmatically read the parameters from the RunState?
+		// TODO: Add validation of this number against the actual number of distributed agent models
+		DefaultParameters defaultParameters = new DefaultParameters();
+		//JZombies
+		if (frameworkConfigurationFileName.equals("PLACEHOLDER_FOR_CSF_CONFIGURATION_FILE")) {
+		defaultParameters.addParameter("human_count", "Human Count", Number.class, 6,
+				true);
+		defaultParameters.addParameter("zombie_count", "Zombie Count", Number.class, 6,
+				true);
+		}
+		//Prisoner's Dilemma
+		else {
+			defaultParameters.addParameter("player_count", "Player Count", Number.class, 8,
+					true);
+			defaultParameters.addParameter("number_of_rounds", "Number of Rounds", Number.class, 10,
+					true);
+			
+		}
+			
+		controller.runParameterSetters(defaultParameters);
+
+		// If Common Framework configuration file is provided, initialize Common
+		// Framework
+		// otherwise run the simulation as a regular Repast simulation
+		// (programmatically).
+		if (frameworkConfigurationFileName != null) {
+			// Call the concrete Adapter as this Adapter is only for Repast
+			// Simphony
+			repastS_SimulationAdapterAPI = RepastS_SimulationAdapterAPI.getInstance();
+			repastS_SimulationRunGroupContext = repastS_SimulationAdapterAPI
+					.initializeAPI(frameworkConfigurationFileName);
+			simulationRunnerType = SIMULATION_TYPE.CSF_SIMULATION;
+		} else
+			simulationRunnerType = SIMULATION_TYPE.NON_CSF_SIMULATION;
+	}
+
+	public SIMULATION_TYPE getSimulationRunnerType() {
+		return simulationRunnerType;
+	}
+
+	/*
+	 * Initializes a single simulation run. Called after the simulation and (if
+	 * configured) Common Simulation Framework are initialized.
+	 */
+	public RepastS_SimulationRunContext runInitialize() {
+		// Set the Seed Parameter for this simulation run
+		// HARD CODED FOR NOW
+		// TODO: Programmatically read the parameters
+		DefaultParameters defaultParameters = new DefaultParameters();
+		defaultParameters.addParameter(ParameterConstants.DEFAULT_RANDOM_SEED_USAGE_NAME,
+				ParameterConstants.DEFAULT_RANDOM_SEED_DISPLAY_NAME, Number.class, 1,
+				true);
+
+		controller.runInitialize(defaultParameters);
+		schedule = RunState.getInstance().getScheduleRegistry().getModelSchedule();
+
+		@SuppressWarnings("unchecked")
+		Context<Object> repastContextForThisRun = RunState.getInstance()
+				.getMasterContext();
+
+		RepastS_SimulationRunContext repastS_SimulationRunContext = null;
+		if (simulationRunnerType == SIMULATION_TYPE.CSF_SIMULATION) {
+			repastS_SimulationRunContext = repastS_SimulationAdapterAPI
+					.initializeSimulationRun(repastContextForThisRun,
+							repastS_SimulationRunGroupContext);
+			// Fix after expanding to support multiple distributed systems.
+			System.out.println(repastS_SimulationRunContext
+					.getSimulationDistributedSystemManagers().iterator().next()
+					.logHelper());
+
+			// TODO: Better handling for future multithreading. Set the interface at
+			// the run group
+			// level, close after all runs have executed.
+
+			lastRepastS_SimulationRunContext = repastS_SimulationRunContext;
+
+			// Now we're ready to start sending the tick information
+		}
+
+		return repastS_SimulationRunContext;
+	}
+
+	public void cleanUpRun() {
+		controller.runCleanup();
+		isStopped = false; // Clear this flag for the next simulation run
+		if (lastRepastS_SimulationRunContext != null) { // if CSF run
+			lastRepastS_SimulationRunContext
+					.closeInterface(lastRepastS_SimulationRunContext
+							.getSimulationRunContext());
+			lastRepastS_SimulationRunContext.terminateSimulationRun();
+		}
+	}
+
+	public void cleanUpBatch() {
+		controller.batchCleanup();
+	}
+
+	// returns the tick count of the next scheduled item
+	public double getNextScheduledTime() {
+		return ((Schedule) RunEnvironment.getInstance().getCurrentSchedule())
+				.peekNextAction().getNextTime();
+	}
+
+	// returns the number of model actions on the schedule
+	public int getModelActionCount() {
+		return schedule.getModelActionCount();
+	}
+
+	// returns the number of non-model actions on the schedule
+	public int getActionCount() {
+		return schedule.getActionCount();
+	}
+
+	// Step the schedule
+	public void step() {
+		// System.out.println("Step");
+		schedule.execute();
+	}
+
+	// stop the schedule
+	public void stop() {
+		if (schedule != null) {
+			isStopped = true;
+			schedule.executeEndActions();
+		}
+	}
+
+	// Called after the last step in the simulation run executes.
+	public void setFinishing(boolean fin) {
+		schedule.setFinishing(fin);
+	}
+
+	// Comment is from the Repast development team
+	public void execute(RunState toExecuteOn) {
+		// required AbstractRunner stub. We will control the
+		// schedule directly.
+	}
+
+	public boolean getIsStopped() {
+		return isStopped;
+	}
+
+	/*
+	 * public void csfPreStep(RepastS_SimulationRunContext repastS_SimulationRunContext) {
+	 * repastS_SimulationRunContext.perceiveGlobalEnvironment();
+	 * 
+	 * }
+	 */
+
+}
